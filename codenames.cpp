7a7e49a404912b4e7f834cad0f46b06864cb38f4
Simon Lindholm <simon.lindholm10@gmail.com>
2016-11-27 20:56:52 +0100

    Use floats instead of doubles

--- codenames.cpp
+++ codenames.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define all(v) (v).begin(), (v).end()
 #define what_is(x) cout << #x << " is " << x << endl;
 
-typedef double fl;
+typedef float fl;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
@@ -22,17 +22,17 @@ void eraseFromVector(string word, vector<string>* v){
 string toLowerCase(string s){
 	rep(i,0,s.size())
 		if(s[i] >= 'A' && s[i] <= 'Z')
-			s[i] += 'a'-'A';
+			s[i] = (char)(s[i] + 'a' - 'A');
 	return s;
 }
 
-double sigmoid(double x){
-	return 1.0/(1.0+exp(-x));
+fl sigmoid(fl x){
+	return 1.0f/(1.0f+exp(-x));
 }
 
 struct SimilarityEngine{
 
-	map<string, vector<double> > vec;
+	map<string, vector<fl> > vec;
 	map<string, int> popularity;
 
 	// Returns if successful
@@ -51,7 +51,7 @@ struct SimilarityEngine{
 		char buf[bufSize];
 		string word;
 		vector<float> values(dimension);
-		vector<double> valuesd;
+		vector<fl> valuesd;
 		rep(i,0,numberOfWords) {
 			int len;
 			fin.read((char*)&len, sizeof len);
@@ -87,8 +87,8 @@ struct SimilarityEngine{
 		return ret;
 	}
 
-	double similarity(const vector<double>& v1, const vector<double>& v2) {
-		double ret=0;
+	fl similarity(const vector<fl>& v1, const vector<fl>& v2) {
+		fl ret=0;
 		int dim = (int)v1.size();
 		rep(i,0,dim) {
 			ret += v1[i] * v2[i];
@@ -96,25 +96,25 @@ struct SimilarityEngine{
 		return ret;
 	}
 
-	double similarity(const string& s1, const string& s2){
+	fl similarity(const string& s1, const string& s2){
 		return similarity(vec.at(s1), vec.at(s2));
 	}
 
-	const vector<double>& getVec(const string& s) {
+	const vector<fl>& getVec(const string& s) {
 		return vec.at(s);
 	}
 
-	vector<pair<double, string> > similarWords(string s){
+	vector<pair<fl, string> > similarWords(string s){
 		if(!vec.count(s)){
 			cout << s << " does not occur in the corpus" << endl;
-			return vector<pair<double, string> >();
+			return vector<pair<fl, string> >();
 		}
-		vector<pair<double, string> > ret;
+		vector<pair<fl, string> > ret;
 		for(auto it=vec.begin(); it != vec.end(); ++it){
 			ret.push_back(make_pair(-similarity(s, it->first), it->first));
 		}
 		sort(all(ret));
-		vector<pair<double, string> > res;
+		vector<pair<fl, string> > res;
 		rep(i,0,10)
 			res.push_back(make_pair(-ret[i].first, ret[i].second));
 		return res;
@@ -125,42 +125,42 @@ struct SimilarityEngine{
 struct Bot{
 
 	// Give a similarity bonus to "bad" words
-	double marginOpponentWords = 0.02;
-	double marginAssassins = 0.05;
+	fl marginOpponentWords = 0.02f;
+	fl marginAssassins = 0.05f;
 
 	// Constants used in scoring function based
 	// on the sigmoid function of the similarities
-	double fuzzyWeightAssassin = -1.0;
-	double fuzzyWeightOpponent = -0.5;
-	double fuzzyWeightMy = 0.5;
-	double fuzzyWeightGrey = -0.1;
-	double fuzzyExponent = 15;
-	double fuzzyOffset = 0.3;
+	fl fuzzyWeightAssassin = -1.0f;
+	fl fuzzyWeightOpponent = -0.5f;
+	fl fuzzyWeightMy = 0.5f;
+	fl fuzzyWeightGrey = -0.1f;
+	fl fuzzyExponent = 15;
+	fl fuzzyOffset = 0.3f;
 
 	// Assume that we will never succeed if the similarity
 	// is at most minSimilarity
-	double minSimilarity = 0.2;
+	fl minSimilarity = 0.2f;
 
 	// How bad is it if there is an opponent word with high similarity
-	double weightOpponent = -1.5;
+	fl weightOpponent = -1.5f;
 	// How bad is it if there is a grey word with high similarity
-	double weightGrey = -0.2;
+	fl weightGrey = -0.2f;
 
 	// How important is it that the last good word has greater
 	// similarity than the next bad word
-	double marginWeight = 0.2;
+	fl marginWeight = 0.2f;
 
 	// Number of words that are considered common
 	int commonWordLimit = 5000;
 
 	// Prefer common words
-	double commonWordWeight = 1.2;
+	fl commonWordWeight = 1.2f;
 
 	// Number of words that are not considered rare
 	int rareWordLimit = 15000;
 
 	// Avoid rare words
-	double rareWordWeight = 0.8;
+	fl rareWordWeight = 0.8f;
 
 	// Consider only the 50000 most common words
 	int vocabularySize = 50000;
@@ -173,14 +173,14 @@ struct Bot{
 	struct BoardWord {
 		char type;
 		string word;
-		vector<double> vec;
+		vector<fl> vec;
 	};
 	vector<BoardWord> boardWords;
 	void addBoardWord(char type, const string& word) {
 		boardWords.push_back({type, word, engine.getVec(word)});
 	}
 
-	pair<double, int> getWordScore(const string& word, bool debugPrint) {
+	pair<fl, int> getWordScore(const string& word, bool debugPrint) {
 		if(debugPrint)
 			cout << "Printing statistics for \"" << word << "\"" << endl;
 
@@ -192,13 +192,13 @@ struct Bot{
 				return make_pair(-1000, -1);
 		}
 
-		const vector<double>& wordVec = engine.getVec(word);
+		const vector<fl>& wordVec = engine.getVec(word);
 
-		typedef pair<double, BoardWord*> Pa;
+		typedef pair<fl, BoardWord*> Pa;
 		static vector<Pa> v;
 		v.clear();
 		rep(i,0,boardWords.size()){
-			double sim=engine.similarity(boardWords[i].vec, wordVec);
+			fl sim=engine.similarity(boardWords[i].vec, wordVec);
 			if(boardWords[i].type == 'o')
 				sim += marginOpponentWords;
 			if(boardWords[i].type == 'a')
@@ -206,11 +206,11 @@ struct Bot{
 			v.push_back(make_pair(-sim, &boardWords[i]));
 		}
 		sort(all(v), [&](const Pa& a, const Pa& b) { return a.first < b.first; });
-		double bestScore=0;
+		fl bestScore=0;
 		int bestCount=0;
-		double curScore=0;
-		double lastGood=0;
-		double baseScore=0;
+		fl curScore=0;
+		fl lastGood=0;
+		fl baseScore=0;
 		int curCount=0;
 		if(debugPrint){
 			rep(i,0,v.size()){
@@ -229,12 +229,13 @@ struct Bot{
 		// Compute a fuzzy score
 		rep(i,0,v.size()){
 			char type = v[i].second->type;
-			double weight;
+			fl weight;
 			if (type == 'a') weight = fuzzyWeightAssassin;
-			if (type == 'o') weight = fuzzyWeightOpponent;
-			if (type == 'm') weight = fuzzyWeightMy;
-			if (type == 'g') weight = fuzzyWeightGrey;
-			double contribution = weight * sigmoid((-v[i].first - fuzzyOffset) * fuzzyExponent);
+			else if (type == 'o') weight = fuzzyWeightOpponent;
+			else if (type == 'm') weight = fuzzyWeightMy;
+			else if (type == 'g') weight = fuzzyWeightGrey;
+			else assert(0);
+			fl contribution = weight * sigmoid((-v[i].first - fuzzyOffset) * fuzzyExponent);
 			baseScore += contribution;
 		}
 
@@ -257,7 +258,7 @@ struct Bot{
 				curScore += weightGrey;
 				continue;
 			}
-			double tmpScore=-1;
+			fl tmpScore=-1;
 			rep(j,i+1,v.size()){
 				char type2 = v[j].second->type;
 				if(type2 == 'a' || type2 == 'o') {
@@ -296,14 +297,14 @@ struct Bot{
 		greyWords = _greyWords;
 		assassinWords = _assassinWords;
 		createBoardWords();
-		vector<pair<double, pair<int, const string*>>> v;
+		vector<pair<fl, pair<int, const string*>>> v;
 		const string* bestWord=0;
-		int bestCount;
-		double bestScore=0;
+		int bestCount=-1;
+		fl bestScore=0;
 		vector<const string*> candidates = engine.getCommonWords(vocabularySize);
 		v.reserve(candidates.size());
 		for(const string* candidate : candidates){
-			pair<double, int> res=getWordScore(*candidate, false);
+			pair<fl, int> res=getWordScore(*candidate, false);
 			v.push_back(make_pair(-res.first, make_pair(res.second, candidate)));
 			if(res.first > bestScore){
 				bestScore=res.first;
@@ -318,10 +319,10 @@ struct Bot{
 
 		// Print a list with the best clues
 		rep(i,0,20){
-			pair<double, int> res = getWordScore(*v[i].second.second, false);
+			pair<fl, int> res = getWordScore(*v[i].second.second, false);
 			cout << (i+1) << "\t" << setprecision(3) << fixed << res.first << "\t" << *v[i].second.second << " " << res.second << endl;
 		}
-		int p=engine.popularity[*bestWord];
+		int p = engine.popularity.at(*bestWord);
 		cout << "The best clue found is " << *bestWord << " " << bestCount << endl;
 		cout << *bestWord << " is the " << p;
 		if(p % 10 == 1 && p % 100 != 11)
