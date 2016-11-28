a649b66892ec809058f01433082fbf8817d3edb5
Simon Lindholm <simon.lindholm10@gmail.com>
2016-11-29 00:13:37 +0100

    Ensure words from the wordlist are included in the model

--- preprocess.cpp
+++ preprocess.cpp
@@ -14,39 +14,61 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vpi;
 
-void processWord2Vec(const char* inFile, const char* outFile, int limit) {
+void processWord2Vec(const char* inFile, const char* outFile, const char* wordlistFile, int limit) {
+	string line;
+	set<string> wordlist;
+	ifstream fin(wordlistFile);
+	if (fin) {
+		while (getline(fin, line)) {
+			wordlist.insert(line);
+		}
+		fin.close();
+	}
+	else {
+		cerr << "Warning: missing wordlist.txt, so unable to ensure all words from there are available." << endl;
+	}
+
 	struct Word {
 		string word;
 		vector<float> vec;
 	};
 
-	ifstream fin(inFile);
+	fin.open(inFile);
 	fin.exceptions(fin.badbit | fin.failbit);
 	assert(fin);
 	vector<Word> words;
-	string line;
 	int dim = -1, count = 0;
 	while (getline(fin, line)) {
 		istringstream iss(line);
 		Word w;
 		iss >> w.word;
+		if (count >= limit && !wordlist.count(w.word))
+			continue;
 		double length = 0;
 		double x;
 		if (dim != -1)
 			w.vec.reserve(dim);
 		while (iss >> x) {
-			w.vec.push_back(x);
+			w.vec.push_back((float)x);
 			length += x*x;
 		}
 		if (dim == -1) dim = sz(w.vec);
 		else assert(sz(w.vec) == dim);
 		length = 1 / sqrt(length);
-		trav(x, w.vec) x *= length;
+		trav(x, w.vec) x = (float)(x * length);
 		words.push_back(w);
+		wordlist.erase(w.word);
 		count++;
-		if (count == limit) break;
+		if (count >= limit && wordlist.empty())
+			break;
 	}
 	fin.close();
+
+	if (!wordlist.empty()) {
+		cerr << "Warning: words not found:" << endl;
+		for (const string &w : wordlist)
+			cerr << w << endl;
+	}
 
 	ofstream fout(outFile, ios::binary);
 	fout.write((char*)&count, sizeof count);
@@ -60,6 +82,12 @@ void processWord2Vec(const char* inFile, const char* outFile, int limit) {
 	fout.close();
 }
 
-int main() {
-	processWord2Vec("data.txt", "data.bin", 100000);
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		cerr << "Usage: " << argv[0] << " <word2vec .txt file> <limit>" << endl;
+		return 1;
+	}
+	const char* inFile = argv[1];
+	int limit = atoi(argv[2]);
+	processWord2Vec(inFile, "data.bin", "wordlist.txt", limit);
 }
