f7569be6decf2e7dd58fea5ce46c79a54037529e
Aron Granberg <aron.granberg@gmail.com>
2017-08-22 16:55:29 +0200

    Generate temporary data in /tmp/codenames/

--- evaluate_model.sh
+++ evaluate_model.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 results="0"
 for x in {1..10}; do
-	python3 training_tf.py train${x}.txt test${x}.txt results${x}.txt > /dev/null &
+	python3 training_tf.py /tmp/codenames/train${x}.txt /tmp/codenames/test${x}.txt /tmp/codenames/results${x}.txt > /dev/null &
 done
 
 wait
 
 for x in {1..10}; do
-	results="$results + `cat results${x}.txt`"
+	results="$results + `cat /tmp/results${x}.txt`"
 	echo "Objective function: `echo $results | bc`"
 done
 echo "Objective function: `echo $results | bc`"
--- extract_features.sh
+++ extract_features.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+mkdir -p /tmp/codenames
 for x in {1..10}
 do
-	cat test_data/ordered5/*.txt | ./codenames --extract-features train${x}.txt test${x}.txt ${x}
+	cat test_data/ordered5/*.txt | ./codenames --extract-features /tmp/codenames/train${x}.txt /tmp/codenames/test${x}.txt ${x}
 done
